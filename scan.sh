dcdd7de396d26bce28d654994a1c759eec133153
Lennox Fei <feif4@mcmaster.ca>
2018-03-23 14:30:23 -0400

    Wrote double the lines just so it looks nicer...

--- scan.sh
+++ scan.sh
@@ -6,21 +6,47 @@ do
     terms+=($line)  
 done < $1
 
-for i in $(ls)
+files=$(ls)
+
+for i in $files
 do
     for term in ${terms[*]}
     do
-	grep $term -irn $i >> _log_"$i"_$term
+	grep $term -irn $i >> "$i"_"$term".scn
     done
 done
 
-mkdir scanlogs
-mv _log* scanlogs/
+scanlogs=${2:scanlogs}
 
-cd scanlogs
-for i in $(ls)
+mkdir $scanlogs
+mv *.scn "$scanlogs"/
+
+cd $scanlogs
+
+echo -ne 'Files\t' >> temp.rpt
+
+for term in ${terms[*]}
 do
-    wc -l $i >> Report
+    echo -ne $term'\t' >> temp.rpt
 done
 
-cat Report
+echo 'Total' >> temp.rpt
+
+for f in $files
+do
+    echo -ne "$f"'\t' >> temp.rpt
+    i=0
+    for term in ${terms[*]}
+    do
+	s=$(wc -l < "$f"_"$term".scn)
+	echo -ne "$s"'\t' >> temp.rpt
+	((i+=s))
+    done
+    echo $i >> temp.rpt
+done
+
+out=${3:Report}
+
+cat temp.rpt | column -t > $out
+rm temp.rpt
+cat $out
